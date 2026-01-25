a3120e7c788c186eac9ce8d0155f2ea7e956b4c4
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-01-25 18:08:10 +0300

    Created file "mem.h" for memory simulation

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
-typedef unsigned char byte_t;
-typedef unsigned short int word_t;
-typedef word_t address_t;
-
+#include "mem.h"
 
 
 int main(int argc, char **argv)
---
+++ mem.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+#define MEMSIZE         (64*1024)
+
+typedef unsigned char byte_t;
+typedef unsigned short int word_t;
+typedef word_t address_t;
+
+//byte_t mem[MEMSIZE];
+word_t mem[MEMSIZE];
+
+
