055d821ffe36a7eabf760fcc5c96744ac30cb42b
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-01-26 19:22:20 +0300

    Added APIs for working with memory

--- mem.h
+++ mem.h
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 #define MEMSIZE         (64*1024)
@@ -10,4 +9,7 @@ typedef word_t address_t;
 //byte_t mem[MEMSIZE];
 word_t mem[MEMSIZE];
 
-
+void b_write(address_t adr, byte_t val); //пишем  значение (byte_t)val по адресу adr;
+byte_t b_read(address_t adr);            // читаем байт по adr и возвращаем его;
+void w_write(address_t adr, word_t val); // пишем значение (слово) val по адрессу adr;
+word_t w_read(address_t adr);            // читаем слово по адрессу adr и возвращаем его;
