d1a0ac16dfcbc295404c9a8e3667cdc7aa26adcc
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-01-30 10:25:31 +0300

    Remamed pdp_11_emu.c main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include "pdp_11/pdp_11.h"
 #include "memory/mem.h"
 #include "tests/test.h"
 
 int main(int argc, char **argv)
 {
-        test_mem();
+        pdp_11_t* pdp = pdp_create();
+
+        test_mem(*pdp);
+
+        pdp_destroy(*pdp);
+
         return 0;
 }
