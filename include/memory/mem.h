0ad0082334b71468fbf523385741bfdca5582636
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-01-31 12:24:27 +0300

    Created a memory structure
    // По условию задачи необходимо
    // создать два вида памяти записи
    // побайтно и машино слова.
    
    // Для обеспечения более точных подсчетов.

--- include/memory/mem.h
+++ include/memory/mem.h
@@ -1,22 +1,24 @@
 #ifndef MEM_H
 #define MEM_H
 
-#include <stdio.h>
+/*#define MEMSIZE         (64*1024)*/
 
+struct byte_t;
+struct word_t;
+struct address_byte_t;
+struct address_word_t;
 
+struct mem_t;
 
-#define MEMSIZE         (64*1024)
+struct mem_t* mem_new();
 
-typedef unsigned char byte_t;
-typedef unsigned short int word_t;
-typedef word_t address_t;
+void mem_create(struct mem_t*, size_t*);
+void mem_destroy(struct mem_t*);
 
-byte_t mem_b[MEMSIZE];
-word_t mem_w[MEMSIZE];
+void b_write(struct mem_t*, const address_byte_t*, const struct byte_t*); //пишем  значение (byte_t)data по адресу adr;
+struct byte_t b_read(struct mem_t*, const address_byte_t*);            // читаем байт по adr и возвращаем его;
 
-void b_write(address_t adr, byte_t val); //пишем  значение (byte_t)val по адресу adr;
-byte_t b_read(address_t adr);            // читаем байт по adr и возвращаем его;
-void w_write(address_t adr, word_t val); // пишем значение (слово) val по адрессу adr;
-word_t w_read(address_t adr);            // читаем слово по адрессу adr и возвращаем его;
+void w_write(struct mem_t*, const address_word_t*, const struct word_t*); // пишем значение (слово) val по адрессу adr;
+struct word_t w_read(struct mem_t*, const address_word_t*);            // читаем слово по адрессу adr и возвращаем его;
 
 #endif
---
+++ include/memory/mem_p.h
@@ -0,0 +1,10 @@
+#ifndef MEM_P_H
+#define MEM_P_H
+
+typedef struct {
+        mem_bite_t* buf_b;
+        mem_word_t* buf_w;
+} mem_t;
+
+#endif
+
