2ad20f5aadeb62491e400c690412b209d3c69782
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-01 11:42:52 +0300

    Starting create the pdp_11 emulator

--- src/main.c
+++ src/main.c
@@ -3,13 +3,16 @@
 #include "memory/mem.h"
 #include "tests/test.h"
 
+size_t memory_byte_size = 65536;
+size_t memory_word_size = 65536;
+
 int main(int argc, char **argv)
 {
-        pdp_11_t* pdp = pdp_create();
+        struct pdp_11_t* pdp = pdp_new();
+        pdp_create(pdp, memory_word_size, memory_word_size);
+        test_mem(pdp);
 
-        test_mem(*pdp);
-
-        pdp_destroy(*pdp);
+        pdp_destroy(pdp);
 
         return 0;
 }
