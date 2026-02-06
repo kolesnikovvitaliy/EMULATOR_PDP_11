4c999c87bc2476d2d080e1a9c79c8135e6649bca
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-06 23:19:04 +0300

    Added header file for creating virtual functions

---
+++ include/memory/type_func_p.h
@@ -0,0 +1,9 @@
+#ifndef TYPES_FUNC_P
+#define TYPES_FUNC_P
+
+typedef byte_t (*read_byte_t)(address_byte_t);
+typedef void (*write_byte_t)(address_byte_t, byte_t);
+typedef word_t (*read_word_t)(address_word_t);
+typedef void (*write_word_t)(address_word_t, word_t);
+
+#endif
