f9b64b5f6dfd84f637e2d03d7126b1f35c80c5e8
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-07 19:53:00 +0300

    Setting up the memory structure

--- include/memory/mem.h
+++ include/memory/mem.h
@@ -1,24 +1,20 @@
 #ifndef MEM_H
 #define MEM_H
 
-/*#define MEMSIZE         (64*1024)*/
+#include "types/types.h"
 
-struct byte_t;
-struct word_t;
-struct address_byte_t;
-struct address_word_t;
 
 struct mem_t;
 
 struct mem_t* mem_new();
 
-void mem_create(struct mem_t*, size_t*);
+void mem_create(struct mem_t*);
 void mem_destroy(struct mem_t*);
 
-void b_write(struct mem_t*, const address_byte_t*, const struct byte_t*); //пишем  значение (byte_t)data по адресу adr;
-struct byte_t b_read(struct mem_t*, const address_byte_t*);            // читаем байт по adr и возвращаем его;
+void byte_write(struct mem_t*, address_byte_t, byte_t); //пишем  значение (byte_t)data по адресу adr;
+byte_t byte_read(struct mem_t*, address_byte_t);            // читаем байт по adr и возвращаем его;
 
-void w_write(struct mem_t*, const address_word_t*, const struct word_t*); // пишем значение (слово) val по адрессу adr;
-struct word_t w_read(struct mem_t*, const address_word_t*);            // читаем слово по адрессу adr и возвращаем его;
+void word_write(struct mem_t*, address_word_t, word_t); // пишем значение (слово) val по адрессу adr;
+word_t word_read(struct mem_t*, address_word_t);            // читаем слово по адрессу adr и возвращаем его;
 
 #endif
--- include/memory/mem_p.h
+++ include/memory/mem_p.h
@@ -1,10 +1,16 @@
 #ifndef MEM_P_H
 #define MEM_P_H
 
+enum {size_buffer_default = 65536};
+
+#include "buf_byte/mem_byte_p.h"
+#include "buf_word/mem_word_p.h"
+
 typedef struct {
-        mem_bite_t* buf_b;
-        mem_word_t* buf_w;
+        mem_byte_t* mem_byte;
+        mem_word_t* mem_word;
 } mem_t;
+
 
 #endif
 
