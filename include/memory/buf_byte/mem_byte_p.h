cd84f51d3919aa2d4c39b63d8b81072386543452
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-07 19:42:09 +0300

    Customizing Byte-Based Memory Structure Header Files

--- include/memory/buf_byte/mem_byte_p.h
+++ include/memory/buf_byte/mem_byte_p.h
@@ -2,10 +2,16 @@
 #define MEM_BYTE_P_H
 
 #include "types/types.h"
+#include "memory/type_func_p.h"
 
 typedef struct {
         byte_t* buf_b;
         size_byte_buffer size_b;
+
+        read_byte_t read_byte;
+        write_byte_t write_byte;
+        read_word_t read_word;
+        write_word_t write_word;
 } mem_byte_t;
 
 #endif
