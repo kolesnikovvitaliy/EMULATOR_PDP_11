95b157ed560ed1003f85d62ae833c0aa3d9e2571
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-07 20:05:53 +0300

    ADD MODUL MEMORY IN SIMULATOR

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "pdp_11/pdp_11.h"
-#include "memory/mem.h"
+
 #include "tests/test.h"
 
-size_t memory_byte_size = 65536;
-size_t memory_word_size = 65536;
+
 
 int main(int argc, char **argv)
 {
         struct pdp_11_t* pdp = pdp_new();
-        pdp_create(pdp, memory_word_size, memory_word_size);
+        pdp_create(pdp);
         test_mem(pdp);
 
         pdp_destroy(pdp);
--- src/memory/mem.c
+++ src/memory/mem.c
@@ -1,6 +1,52 @@
-#include "memory/mem.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void b_write(address_t addr, byte_t ch)
+#include "memory/mem_p.h"
+#include "types/types.h"
+#include "memory/buf_byte/mem_byte.h"
+#include "memory/buf_word/mem_word.h"
+
+mem_t* mem_new()
 {
-        mem_b[addr] = ch;
+        return (mem_t*)malloc(sizeof(mem_t));
+}
+
+void mem_create(mem_t* memory)
+{
+        memory->mem_byte = (mem_byte_t*)mem_byte_new();
+        mem_byte_create((struct mem_byte_t*)memory->mem_byte, size_buffer_default);
+
+        memory->mem_word = (mem_word_t*)mem_word_new();
+        mem_word_create((struct mem_word_t*)memory->mem_word, size_buffer_default);
+
+}
+
+void mem_destroy(mem_t* memory)
+{
+        mem_byte_destroy((struct mem_byte_t*)memory->mem_byte);
+        mem_word_destroy((struct mem_word_t*)memory->mem_word);
+}
+
+void byte_write(mem_t* memory,
+                address_byte_t addr, byte_t data)
+{
+        memory->mem_byte->write_byte(memory->mem_byte,addr, data);
+}
+
+
+byte_t byte_read(mem_t* memory, address_byte_t addr)
+{
+        return (byte_t)memory->mem_byte->read_byte(memory->mem_byte, addr);
+}
+//#######################################################
+void word_write(mem_t* memory,
+                address_word_t addr, word_t data)
+{
+        memory->mem_word->write_word(memory->mem_byte,addr, data);
+}
+
+
+word_t word_read(mem_t* memory, address_word_t addr)
+{
+        return (word_t)memory->mem_word->read_word(memory->mem_byte,addr);
 }
