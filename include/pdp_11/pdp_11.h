af9d5fe5cb45910d0c98825ced806a8a1521b719
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-07 20:02:43 +0300

    add header files pdp emulator

---
+++ include/pdp_11/pdp_11.h
@@ -0,0 +1,21 @@
+#ifndef PDP_11_H
+#define PDP_11_H
+
+#include "types/types.h"
+
+struct pdp_11_t;
+
+struct pdp_11_t* pdp_new();
+
+void pdp_create(struct pdp_11_t*);
+void pdp_destroy(struct pdp_11_t*);
+
+
+void b_write(struct pdp_11_t* , address_byte_t, byte_t);//пишем  значение (byte_t)data по адресу adr;
+byte_t b_read(struct pdp_11_t* , address_byte_t);
+// читаем байт по adr и возвращаем его;
+void w_write(struct pdp_11_t* , address_word_t, word_t);// пишем значение (слово) val по адрессу adr;
+word_t w_read(struct pdp_11_t* , address_word_t);
+// читаем слово по адрессу adr и возвращаем его;
+
+#endif
---
+++ include/pdp_11/pdp_11_p.h
@@ -0,0 +1,11 @@
+#ifndef PDP_P_H
+#define PDP_P_H
+
+//#include "memory/mem.h"
+
+typedef struct {
+       struct mem_t* memory;
+} pdp_11_t;
+
+#endif
+
