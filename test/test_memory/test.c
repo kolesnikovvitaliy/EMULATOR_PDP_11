812de46f0b4fb6a8a85997a8dae458023126ee43
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-07 20:09:53 +0300

    Add modul TEST

--- test/test_memory/test.c
+++ test/test_memory/test.c
@@ -2,36 +2,36 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#include "memory/mem_p.h"
-#include "pdp_11/pdp_11_p.h"
+#include "pdp_11/pdp_11.h"
 #include "tests/test.h"
-typedef int bool_t;
+#include "types/types.h"
 
 
 //-------------------------------------------------------------------
 /* Записать и проверить байт по адресу */
-bool_t test_b_write(pdp_11_t* pdp, address_byte_t* addr, byte_t* data)
+bool_t test_b_write(struct pdp_11_t* pdp, address_byte_t addr, byte_t data)
 {
 
         b_write(pdp, addr, data);
 
         printf("\nRESULT MEM_B = %x\n", b_read(pdp, addr));
 
-        return 0;
+        return (b_read(pdp, addr) == data);
 }
 
 //-------------------------------------------------------------------
 
 //////////////////////////////////////////////////////////////////////
-void test_mem(pdp_11_t* pdp)
+void test_mem(struct pdp_11_t* pdp)
 {
-        address_byte_t* adr;
+        address_byte_t adr;
         byte_t b0, b1, bres;
-        word_t *w, *wres;
+        word_t w, wres;
 
         adr = 0;
         b0 = 0x12;
-        assert(test_b_write(pdp, adr, &b0));
+
+        assert(test_b_write(pdp, adr, b0) && "ERROR IS INCORRECT");
 }
 //////////////////////////////////////////////////////////////////////
 
