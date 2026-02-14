68895cefbbc5310afc3cda425e63bb99b2076791
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-14 16:14:48 +0300

    Formatted the modul buf_bute.c

--- src/memory/buf_byte/buf_byte.c
+++ src/memory/buf_byte/buf_byte.c
@@ -4,47 +4,21 @@
 #include "memory/buf_byte/mem_byte_p.h"
 #include "memory/type_func_p.h"
 
-/////////////////////////////////////////////////////////////////////
-//-----------------------------------------------------------------------
-void __word_write_b(void* mem_byte,
-                address_word_t addr,
-                word_t data)
-{
-        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
 
-        *(ptr->buf_b + addr) = data & 0xFF;
-        *(ptr->buf_b + addr + 1) =  (data >> 8) & 0xFF;
-}
+//--------------------------------------------------------------
+void __byte_write_b(void*, address_byte_t, byte_t);
+byte_t __byte_read_b(void* , address_byte_t);
+word_t __word_read_b(void*, address_word_t);
+void __word_write_b(void*, address_word_t, word_t);
+//--------------------------------------------------------------
 
-word_t
-__word_read_b(void* mem_byte, address_word_t addr)
-{
-        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
-
-        return (*(ptr->buf_b + addr) | *(ptr->buf_b + addr + 1) << 8);
-}
-
-void __byte_write_b(void* mem_byte,
-               address_byte_t addr,
-               byte_t data)
-{
-        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
-        *(ptr->buf_b + addr) = data;
-}
-
-byte_t
-__byte_read_b(void* mem_byte, address_byte_t addr)
-{
-        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
-        return *(ptr->buf_b + addr);
-}
-//-----------------------------------------------------------------------
 /////////////////////////////////////////////////////////////////////
 
 mem_byte_t* mem_byte_new()
 {
        return (mem_byte_t*)malloc(sizeof(mem_byte_t));
 }
+
 void mem_byte_create(mem_byte_t* mem_byte, size_byte_buffer size)
 {
         mem_byte->buf_b = (byte_t*)calloc(size, sizeof(byte_t));
@@ -63,3 +37,42 @@ void mem_byte_destroy(mem_byte_t* mem_byte)
 }
 
 /////////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------
+void __word_write_b(void* mem_byte,
+                address_word_t addr,
+                word_t data)
+{
+        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
+
+        *(ptr->buf_b + addr) = data & 0xFF;
+        *(ptr->buf_b + addr + 1) =  (data >> 8) & 0xFF;
+}
+
+//-----------------------------------------------------------------------
+word_t
+__word_read_b(void* mem_byte, address_word_t addr)
+{
+        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
+
+        return (*(ptr->buf_b + addr) | *(ptr->buf_b + addr + 1) << 8);
+}
+//-----------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------
+void __byte_write_b(void* mem_byte,
+               address_byte_t addr,
+               byte_t data)
+{
+        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
+        *(ptr->buf_b + addr) = data;
+}
+
+//-----------------------------------------------------------------------
+byte_t
+__byte_read_b(void* mem_byte, address_byte_t addr)
+{
+        mem_byte_t *ptr = (mem_byte_t*)mem_byte;
+        return *(ptr->buf_b + addr);
+}
+//-----------------------------------------------------------------------
--- src/memory/buf_word/buf_word.c
+++ src/memory/buf_word/buf_word.c
@@ -4,43 +4,15 @@
 #include "memory/buf_word/mem_word_p.h"
 #include "memory/type_func_p.h"
 
-/////////////////////////////////////////////////////////////////////
-void __word_write_w(void* mem_word,
-                address_word_t addr,
-                word_t data)
-{
-        mem_word_t *ptr = (mem_word_t*)mem_word;
 
-        *(ptr->buf_w + addr) = data & 0xFF;
-        *(ptr->buf_w + addr + 1) =  (data >> 8) & 0xFF;
-}
+//--------------------------------------------------------------
+void __byte_write_w(void*, address_byte_t, byte_t);
+byte_t __byte_read_w(void* , address_byte_t);
+word_t __word_read_w(void*, address_word_t);
+void __word_write_w(void*, address_word_t, word_t);
+//--------------------------------------------------------------
 
-word_t
-__word_read_w(void* mem_word, address_word_t addr)
-{
-        mem_word_t *ptr = (mem_word_t*)mem_word;
-
-        return (*(ptr->buf_w + addr) | *(ptr->buf_w + addr + 1) << 8);
-}
-/////////////////////////////////////////////////////////////////////
-void __byte_write_w(void* mem_word,
-               address_byte_t addr,
-               byte_t data)
-{
-        mem_word_t *ptr = (mem_word_t*)mem_word;
-
-        *(ptr->buf_w + addr) = data;
-}
-
-byte_t
-__byte_read_w(void* mem_word, address_byte_t addr)
-{
-        mem_word_t *ptr = (mem_word_t*)mem_word;
-
-        return *(ptr->buf_w + addr);
-}
-
-///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 mem_word_t* mem_word_new()
 {
         return (mem_word_t*)malloc(sizeof(mem_word_t));
@@ -61,3 +33,46 @@ void mem_word_destroy(mem_word_t* mem_word)
 {
         free(mem_word->buf_w);
 }
+/////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------
+void __word_write_w(void* mem_word,
+                address_word_t addr,
+                word_t data)
+{
+        mem_word_t *ptr = (mem_word_t*)mem_word;
+
+        *(ptr->buf_w + addr) = data & 0xFF;
+        *(ptr->buf_w + addr + 1) =  (data >> 8) & 0xFF;
+}
+
+//------------------------------------------------------------------
+
+word_t
+__word_read_w(void* mem_word, address_word_t addr)
+{
+        mem_word_t *ptr = (mem_word_t*)mem_word;
+
+        return (*(ptr->buf_w + addr) | *(ptr->buf_w + addr + 1) << 8);
+}
+//------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------
+void __byte_write_w(void* mem_word,
+               address_byte_t addr,
+               byte_t data)
+{
+        mem_word_t *ptr = (mem_word_t*)mem_word;
+
+        *(ptr->buf_w + addr) = data;
+}
+
+//------------------------------------------------------------------
+byte_t
+__byte_read_w(void* mem_word, address_byte_t addr)
+{
+        mem_word_t *ptr = (mem_word_t*)mem_word;
+
+        return *(ptr->buf_w + addr);
+}
+//------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////
