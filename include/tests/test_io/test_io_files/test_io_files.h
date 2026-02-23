4624ebbe2b3b8220c3e82ce58f164dfc688fb74d
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-23 17:23:26 +0300

    Added settings test PDP_EMULATOR_IO

---
+++ include/tests/test_io/test_io_files/test_io_files.h
@@ -0,0 +1,11 @@
+#ifndef TEST_IO_FILES_H
+#define TEST_IO_FILES_H
+
+#include "pdp_11/pdp_11.h"
+//#include "types/types.h"
+
+
+int test_io_files(struct pdp_11_t*,const byte_t*, const byte_t*);
+
+#endif
+#pragma once
---
+++ include/tests/test_io/test_io_terminal/test_io_terminal.h
@@ -0,0 +1,11 @@
+#ifndef TEST_IO_TERMINAL_H
+#define TEST_IO_TERMINAL_H
+
+#include "pdp_11/pdp_11.h"
+//#include "types/types.h"
+
+
+int test_io_terminal(struct pdp_11_t*);
+
+#endif
+#pragma once
