57505d12f88a52a3d8022e05793c9dde2c941255
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-24 20:56:50 +0300

    Architecture: Moved header files TEST in test_pdp

--- include/tests/test_pdp/test_io/test_io.h
+++ include/tests/test_pdp/test_io/test_io.h
@@ -1,8 +1,8 @@
 #ifndef TEST_IO_H
 #define TEST_IO_H
 
-#include "test_io_files/test_io_files.h"
-#include "test_io_terminal/test_io_terminal.h"
+#include "./test_io_files/test_io_files.h"
+#include "./test_io_terminal/test_io_terminal.h"
 #include "pdp_11/pdp_11.h"
 
 int test_io(struct pdp_11_t*, int, char**);
--- include/tests/test_pdp/test_io/test_io_files/test_io_files.h
+++ include/tests/test_pdp/test_io/test_io_files/test_io_files.h
@@ -5,7 +5,7 @@
 //#include "types/types.h"
 
 
-int test_io_files(struct pdp_11_t*,const byte_t*, const byte_t*);
+int test_io_files(struct pdp_11_t*, byte_t*);
 
 #endif
 #pragma once
