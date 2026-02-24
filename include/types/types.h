a5c176556fcdb46326e184d8c9f1b976940609bc
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-02-24 20:59:38 +0300

    Architecture: Moved Memoru files  in PDP_11

--- include/types/types.h
+++ include/types/types.h
@@ -1,5 +1,6 @@
 #ifndef TYPES_H
 #define TYPES_H
+#include <stdio.h>
 
 enum {size_buffer_default = 0xFFFF};
 typedef unsigned char byte_t;
@@ -12,7 +13,6 @@ typedef unsigned int address_byte_t;
 
 typedef unsigned int size_byte_buffer;
 typedef unsigned int size_word_buffer;
-address_byte_t __get_size_buffer();
 
 
 #endif
