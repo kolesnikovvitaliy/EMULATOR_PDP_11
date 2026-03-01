de91e2a4ad2037cbd9622141e71d3c1874e5c6ed
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-03-01 14:50:50 +0300

    REFACTOR: Editing, substituting a const parameter

--- include/pdp_11/pdp_11.h
+++ include/pdp_11/pdp_11.h
@@ -11,11 +11,11 @@ void pdp_create(struct pdp_11_t*);
 void pdp_destroy(struct pdp_11_t*);
 
 
-void b_write(struct pdp_11_t* , address_byte_t, byte_t);//пишем  значение (byte_t)data по адресу adr;
-byte_t b_read(struct pdp_11_t* , address_byte_t);// читаем байт по adr и возвращаем его;
+void b_write(struct pdp_11_t* , const address_byte_t, byte_t);//пишем  значение (byte_t)data по адресу adr;
+byte_t b_read(struct pdp_11_t* , const address_byte_t);// читаем байт по adr и возвращаем его;
 
-void w_write(struct pdp_11_t* , address_word_t, word_t);// пишем значение (слово) val по адрессу adr;
-word_t w_read(struct pdp_11_t* , address_word_t);// читаем слово по адрессу adr и возвращаем его;
+void w_write(struct pdp_11_t* , const address_word_t, word_t);// пишем значение (слово) val по адрессу adr;
+word_t w_read(struct pdp_11_t* , const address_word_t);// читаем слово по адрессу adr и возвращаем его;
 
 void pdp_load_data(struct pdp_11_t* pdp, byte_t* filename);
 void pdp_mem_dump(struct pdp_11_t* pdp, address_word_t addr, word_t size);
