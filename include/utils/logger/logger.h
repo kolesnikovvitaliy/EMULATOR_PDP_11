7b1acb6428ab4915b467ac4d1d181ba9f5d8ed43
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-03-16 21:59:32 +0300

    CODE: Add LOGGER

---
+++ include/utils/logger/logger.h
@@ -0,0 +1,31 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+typedef enum {
+        ERROR = 0,
+        INFO,
+        WARNING,
+        TRACE,
+        DEBUG
+} log_level_t;
+
+#define CLR_ERROR "\x1b[31m" // Красный
+#define CLR_INFO  "\x1b[32m" // Зеленый
+#define CLR_WARNING  "\x1b[33m" // Желтый
+#define CLR_TRACE "\x1b[94m" // Светло-синий
+#define CLR_DEBUG "\x1b[36m" // Циан
+#define CLR_RESET "\x1b[0m"
+
+
+void log_message(log_level_t, const char*, int, const char*, const char*, ...);
+log_level_t set_log_level(log_level_t);
+
+// Макросы для удобного вызова
+#define ERROR(fmt, ...) log_message(ERROR, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define INFO(fmt, ...) log_message(INFO, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define WARNING(fmt, ...)  log_message(WARNING,  __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define TRACE(fmt, ...) log_message(TRACE, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+#define DEBUG(fmt, ...) log_message(DEBUG, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+
+#endif
+#pragma once
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "pdp_11/pdp_11.h"
+#include "utils/logger/logger.h"
 #include "pdp_11/run.h"
 #include "tests/test_pdp/test_pdp.h"
 
@@ -10,9 +11,10 @@ int main(int argc, char **argv)
 {
         //test_pdp(argc, argv); // ЗАПУСК ТЕСТОВ;
 
-        // Перед запуском тестов закоментировать;
+        // Перед запуском тестов закомментировать;
         struct pdp_11_t* pdp = pdp_new(); // Выделение памяти под обЬетк PDP_11;
         assert(pdp); // проверка
+        INFO("PDP_11 ИНИЦИАЛИЗИРОВАННА", "");
 
         run(pdp, argc, argv); // Запуск ЭМУЛЯТОРА;
 
