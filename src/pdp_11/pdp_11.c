a05aa807f6bf2b278edde5b6adcfc219ad8a2250
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-04-07 20:51:35 +0300

    CODE: Add comments in pdp_11.c

--- src/pdp_11/pdp_11.c
+++ src/pdp_11/pdp_11.c
@@ -1,3 +1,8 @@
+/**
+ * @file pdp_11.c
+ * @brief Основная реализация эмулятора PDP-11.
+ */
+
 #include "pdp_11/command/command.h"
 #include "pdp_11/command/command_p.h"
 #include "pdp_11/device_io/device_io.h"
@@ -13,131 +18,145 @@
 #include <stdlib.h>
 #include <string.h>
 
-/////////////////////////////////////////////////////
+/**
+ * @brief Создает новый экземпляр структуры PDP-11.
+ * @return Указатель на выделенную память для pdp_11_t.
+ */
 pdp_11_t *
 pdp_new()
 {
-    return (pdp_11_t *) malloc(
-        sizeof(pdp_11_t)); // Выделение памяти под обЬетк PDP_11;
+    return (pdp_11_t *) malloc(sizeof(pdp_11_t));
 }
-//------------------------------------------------------------------;
+
+/**
+ * @brief Инициализирует компоненты PDP-11 (память, устройства ввода-вывода,
+ * регистры, команды).
+ * @param pdp Указатель на экземпляр PDP-11.
+ */
 void
 pdp_create(pdp_11_t *pdp)
 {
-    pdp->memory = (struct mem_t *)
-        mem_new(); // Выделение памяти под обЬетк  памяти типа byte и word;
-    pdp->device_io = (struct dev_io_t *)
-        dev_io_new(); // Выделение памяти под обЬетк ввода вывода;
-    pdp->regist = (struct reg_t *)
-        register_new(); // Выделение памяти под обЬетк Регистры;
-    pdp->command = (struct command_t **) command_new();
-    // Выделение памяти под обЬетк машинные Команды;
-    assert(pdp->regist);    // проверка
-    assert(pdp->memory);    // проверка
-    assert(pdp->device_io); // проверка
-    assert(pdp->command);   // проверка
+    pdp->memory    = (struct mem_t *) mem_new();
+    pdp->device_io = (struct dev_io_t *) dev_io_new();
+    pdp->regist    = (struct reg_t *) register_new();
+    pdp->command   = (struct command_t **) command_new();
 
-    mem_create(pdp->memory); // Инициализация обЬекта памяти
-    dev_io_create(pdp->device_io); // Инициализация обЬекта ввода вывода
-    reg_create((struct pdp_11_t *) pdp,
-               pdp->regist); // Инициализация обЬекта Регистры
-    command_create(pdp->command); // Инициализация обЬекта машинные Команды;
+    assert(pdp->regist);
+    assert(pdp->memory);
+    assert(pdp->device_io);
+    assert(pdp->command);
+
+    mem_create(pdp->memory);
+    dev_io_create(pdp->device_io);
+    reg_create((struct pdp_11_t *) pdp, pdp->regist);
+    command_create(pdp->command);
 }
-//------------------------------------------------------------------;
+
+/**
+ * @brief Освобождает ресурсы, связанные с экземпляром PDP-11.
+ * @param pdp Указатель на экземпляр PDP-11 для уничтожения.
+ */
 void
 pdp_destroy(pdp_11_t *pdp)
 {
-    mem_destroy((struct mem_t *) pdp->memory); // Уничтожение оБьекта память;
-    free(pdp->memory); // Высвобождение памяти;
-    dev_io_destroy((struct dev_io_t *)
-                       pdp->device_io); // Уничтожение оБьекта Ввода-Вывода;
-    free(pdp->device_io); // Высвобождение памяти;
-    reg_destroy((struct pdp_11_t *) pdp); // Уничтожение оБьекта Регистры;
+    mem_destroy((struct mem_t *) pdp->memory);
+    free(pdp->memory);
+    dev_io_destroy((struct dev_io_t *) pdp->device_io);
+    free(pdp->device_io);
+    reg_destroy((struct pdp_11_t *) pdp);
     free(pdp->regist);
-    command_destroy((struct command_t **)
-                        pdp->command); // Уничтожение оБьекта машинные команды;
+    command_destroy((struct command_t **) pdp->command);
     free(pdp->command);
 }
-//////////////////////////////////////////////////////
 
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+/**
+ * @brief Записывает байт в память PDP-11.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param addr Адрес памяти для записи.
+ * @param data Данные (байт) для записи.
+ */
 void
 b_write(pdp_11_t *pdp, address_byte_t addr, byte_t data)
 {
-    /* Записывает байт по указанному адрессу */
     assert(__is_valid_address(addr));
     byte_write((struct mem_t *) pdp->memory, addr, data);
 }
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+
+/**
+ * @brief Читает байт из памяти PDP-11.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param addr Адрес памяти для чтения.
+ * @return Прочитанный байт.
+ */
 byte_t
 b_read(pdp_11_t *pdp, address_byte_t addr)
 {
-    /* Читает байт из указанного адреса */
     assert(__is_valid_address(addr));
     return (byte_t) byte_read((struct mem_t *) pdp->memory, addr);
 }
 
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+/**
+ * @brief Записывает слово (16 бит) в память PDP-11.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param addr Адрес памяти для записи.
+ * @param data Данные (слово) для записи.
+ */
 void
 w_write(pdp_11_t *pdp, address_word_t addr, word_t data)
 {
-    /* Записывает слово по указанному адрессу */
     assert(__is_valid_address(addr));
     word_write((struct mem_t *) pdp->memory, addr, data);
 }
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+
+/**
+ * @brief Читает слово (16 бит) из памяти PDP-11.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param addr Адрес памяти для чтения.
+ * @return Прочитанное слово.
+ */
 word_t
 w_read(pdp_11_t *pdp, address_word_t addr)
 {
-    /* Читает слово из указанного адреса */
     assert(__is_valid_address(addr));
     return (word_t) word_read((struct mem_t *) pdp->memory, addr);
 }
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+
+/**
+ * @brief Загружает данные в PDP-11 из файла или терминала.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param filename Путь к файлу (если NULL, чтение из терминала).
+ */
 void
 pdp_load_data(pdp_11_t *pdp, byte_t *filename)
 {
-    /*
-     * Чтение потока данных из
-     * Терминала или Файла
-     * в зависимости от наличия
-     * пути к файлу
-     */
     dev_io_load_data((struct pdp_11_t *) pdp,
                      (struct dev_io_t *) pdp->device_io,
                      (byte_t *) filename);
 }
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+
+/**
+ * @brief Выводит дамп памяти в форматированном виде.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param addr Начальный адрес.
+ * @param size Количество байт для вывода.
+ */
 void
 pdp_mem_dump(pdp_11_t *pdp, address_word_t addr, word_t size)
 {
-    /*
-     * Форматированный вывод данных из памяти
-     * из указанного адресса
-     * определенного количества байт
-     */
     dev_io_mem_dump((struct pdp_11_t *) pdp, addr, size);
 }
-//------------------------------------------------------------------;
-//------------------------------------------------------------------;
+
+/**
+ * @brief Разбирает аргументы командной строки для поиска имени файла.
+ * @param argc Количество аргументов.
+ * @param argv Массив строк аргументов.
+ * @return Указатель на имя файла или 0, если файл не найден/не указан.
+ */
 byte_t *
 pdp_parse_filename(int argc, char **argv)
 {
-    // Проверка наличия пути к файлу;
     if (1 == argc) {
-
         usage((byte_t *) argv[0]);
-        /*
-         * Указание о возможности
-         * использовать программы с префиксом -t:
-         * "USAGE: ./pdp.out [-t] filename - input data:
-         */
         return (byte_t *) 0;
     }
     if (2 == argc) {
@@ -159,17 +178,25 @@ pdp_parse_filename(int argc, char **argv)
     assert(!argv[0]);
     exit(1);
 }
-//------------------------------------------------------------------;
+
+/**
+ * @brief Выполняет одну машинную команду по указанному адресу.
+ * @param pdp Указатель на экземпляр PDP-11.
+ * @param commands Список доступных команд.
+ * @param addr Адрес команды для выполнения.
+ * @return Указатель на обновленный Program Counter (PC).
+ */
 word_t *
 do_command(pdp_11_t *pdp, command_t **commands, const address_word_t addr)
 {
     extern byte_t commands_list;
     word_t *      ptr_pc = pdp->PC;
     *ptr_pc              = addr;
-    word_t word_command; // текущее слово, которое содержит команду
-    // читаем текущее слово
+    word_t word_command;
+
     byte_t flag  = 0;
     word_command = w_read(pdp, *ptr_pc);
+
     for (int i = 1; i < commands_list; i++) {
         if ((word_command & commands[i]->mask) == commands[i]->opcode) {
             commands[i]->do_commands_command(
@@ -178,14 +205,12 @@ do_command(pdp_11_t *pdp, command_t **commands, const address_word_t addr)
             break;
         }
     }
+
     if (!flag) {
         commands[0]->do_commands_command(
             (struct pdp_11_t *) pdp, addr, word_command);
     }
-    // печатаем адрес и слово по этому адресу, как в листинге
-    // TRACE("%06o %06o: ", *ptr_pc, w);
-    // pc сразу же указывает на следующее неразобранное слово
+
     *ptr_pc += 2;
     return ptr_pc;
 }
-//------------------------------------------------------------------;
