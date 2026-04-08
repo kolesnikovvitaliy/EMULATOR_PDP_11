d130e58e3140c546f92b1d1e567ba7cc761c06e4
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-04-08 17:16:36 +0300

    CODE: Added code in test_command.h + Сборка компьютера

---
+++ include/tests/test_pdp/test_command/test_command.h
@@ -0,0 +1,8 @@
+#ifndef TEST_COMMAND
+#    define TEST_COMMAND
+//#include "pdp_11/pdp_11.h"
+struct pdp_11_t;
+int test_command(struct pdp_11_t);
+
+#endif
+#pragma once
--- include/tests/test_pdp/test_reg/test_reg.h
+++ include/tests/test_pdp/test_reg/test_reg.h
@@ -1,8 +1,8 @@
 #ifndef TEST_REG_H
 #    define TEST_REG_H
 
-#    include "pdp_11/pdp_11.h"
-
+//#    include "pdp_11/pdp_11.h"
+struct pdp_11_t;
 int test_reg(struct pdp_11_t *);
 
 #endif
