279a8035aa24237ec91bd821cdd2d77a70fba2c0
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-04-22 20:51:32 +0300

    CODE: Refactor command mov

--- src/pdp_11/command/command.c
+++ src/pdp_11/command/command.c
@@ -98,38 +98,38 @@ __get_mr(struct pdp_11_t *pdp, word_t word_command)
 
     switch (num_mode) {
     case 0:
-        res.addr  = num_register;
-        res.value = *(ptr_pdp->R0 + ((word_command >> 6) & 7));
+        res.addr  = ((word_command >> 6) & 7);
+        res.value = *(ptr_pdp->R0 + res.addr);
         TRACE("R%d\t ADDR = %06o\t VALUE = %06o\t ",
               num_register,
               res.addr,
               res.value);
         break;
         // мода 1, (R1)
-        ////    case 1:
-        //        res.addr = ;           // в регистре адрес
-        //        res.val = w_read(res.adr);  // по адресу - значение
-        //        trace(TRACE, "(R%d) ", r);
-        //        break;
-        //
-        //
-        //    // мода 2, (R1)+ или #3
-        //    case 2:
-        //        res.adr = reg[r];           // в регистре адрес
-        //        res.val = w_read(res.adr);  // по адресу - значение
-        //        reg[r] += 2;                // TODO: +1
-        //        // печать разной мнемоники для PC и других регистров
-        //        if (r == 7)
-        //            trace(TRACE, "#%o ", res.val);
-        //        else
-        //            trace(TRACE, "(R%d)+ ", r);
-        //        break;
-        //
-        //
-        //    // мы еще не дописали другие моды
-        //    default:
-        //        trace(ERROR, "Mode %d not implemented yet!\n", m);
-        //        exit(1);
+    case 1:
+        res.addr
+            = *(ptr_pdp->R0 + ((word_command >> 6) & 7)); // в регистре адрес
+        res.value = w_read(pdp, res.addr); // по адресу - значение
+        TRACE("CASE 1\tR%d\t ADDR = %06o\t VALUE = %06o\t ",
+              num_register,
+              res.addr,
+              res.value);
+        break;
+        // мода 2, (R1)+ или #3
+        // case 2:
+        //     res.adr = reg[r];           // в регистре адрес
+        //     res.val = w_read(res.adr);  // по адресу - значение
+        //     reg[r] += 2;                // TODO: +1
+        //     // печать разной мнемоники для PC и других регистров
+        //     if (r == 7)
+        //         trace(TRACE, "#%o ", res.val);
+        //     else
+        //         trace(TRACE, "(R%d)+ ", r);
+        //     break;
+        // // мы еще не дописали другие моды
+        // default:
+        //     trace(ERROR, "Mode %d not implemented yet!\n", m);
+        //     exit(1);
     }
     return res;
 }
