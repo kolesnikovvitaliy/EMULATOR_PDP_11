765690d1cca99847d8c80c893ee81a42ab6d4e9a
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-05-02 17:21:56 +0300

    CODE: Refactor dir Commands

---
+++ src/pdp_11/command/commands.c
@@ -0,0 +1,70 @@
+// COMMANDS
+
+void
+command_do_halt(struct pdp_11_t *pdp, address_word_t addr, word_t word_command)
+{
+    (void) addr;
+    (void) word_command;
+    pdp_11_t *ptr_pdp = (pdp_11_t *) pdp;
+    word_t *  ptr_pc  = ptr_pdp->PC;
+    *ptr_pc += 2;
+    command_reg_dump(pdp);
+    // pdp_mem_dump(pdp, 0x40, 0x20);
+    // pdp_mem_dump(pdp, 0x200, 0x26);
+    PRINT_RESULT("THE END!!!\n", "");
+
+    pdp_destroy(pdp);
+    free(pdp);
+    exit(0);
+}
+
+/**
+@brief Выполняет операцию сложения (ADD) для процессора PDP-11.
+Извлекает значения операндов источника (ss) и назначения (dd),
+суммирует их и записывает результат по адресу назначения.
+@param[in,out] pdp Указатель на структуру состояния процессора.
+@param[in] addr Адрес текущей команды (не используется).
+@param[in] word_command 16-битный код команды с параметрами адресации.
+@note Параметр addr приводится к (void) для предотвращения
+предупреждений компилятора.
+*/
+void
+command_do_add(struct pdp_11_t *pdp, address_word_t addr, word_t word_command)
+{
+
+    (void) addr;
+    op_code_t opcode  = { { 0, 0 }, { 0, 0 } };
+    pdp_11_t *ptr_pdp = (pdp_11_t *) pdp;
+    if (pdp)
+        opcode = __get_mr(pdp, word_command);
+    *(ptr_pdp->R0 + opcode.dd.addr) = opcode.ss.value + opcode.dd.value;
+}
+
+void
+command_do_mov(struct pdp_11_t *pdp, address_word_t addr, word_t word_command)
+{
+
+    (void) addr;
+    op_code_t opcode = { { 0, 0 }, { 0, 0 } };
+    if (pdp)
+        opcode = __get_mr(pdp, word_command);
+    pdp_11_t *ptr_pdp               = (pdp_11_t *) pdp;
+    *(ptr_pdp->R0 + opcode.dd.addr) = (word_t) opcode.ss.value;
+}
+
+void
+command_do_inc(struct pdp_11_t *pdp, address_word_t addr, word_t word_command)
+{
+    (void) addr;
+    if (pdp)
+        __get_mr(pdp, word_command);
+}
+
+void
+command_do_unknown(struct pdp_11_t *pdp,
+                   address_word_t   addr,
+                   word_t           word_command)
+{
+    if (pdp)
+        print_command(addr, word_command, (byte_t *) "unknown");
+}
---
+++ src/pdp_11/command/commands_list.c
@@ -0,0 +1,12 @@
+command_t template_commands[] = {
+    { 0000000, 0111111, (byte_t *) "unknown", command_do_unknown, NO_PARAMS },
+    { 0177777, 0000000, (byte_t *) "halt", command_do_halt, NO_PARAMS },
+    { 0170000, 0010000, (byte_t *) "mov", command_do_mov, HAS_SS | HAS_DD },
+    { 0170000, 0060000, (byte_t *) "add", command_do_add, HAS_SS | HAS_DD },
+    { 0107700, 0005200, (byte_t *) "inc", command_do_inc, HAS_SS | HAS_DD }
+
+    // добавить остальные команды
+};
+
+/* размер списка комманд */
+byte_t commands_list = sizeof(template_commands) / sizeof(command_t);
