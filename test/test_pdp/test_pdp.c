5060c6c9a9990fe63c14517cbdbaaf112a543f41
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2026-06-02 22:15:18 +0300

    REFACTOR: START TESTS

--- test/test_pdp/test_pdp.c
+++ test/test_pdp/test_pdp.c
@@ -33,11 +33,6 @@ test_pdp_memory(byte_t type_memory, int argc, char **argv)
 
     memory_type(type_memory);
 
-    /*
-     * // DEBUG("DEFAULT MEMORY FOR CREATE PDP_11 \
-     * //                     \t%d\r\n",
-     * //       g_default_memory);
-     */
 
     struct pdp_11_t *pdp = pdp_new();
     pdp_create(pdp);
